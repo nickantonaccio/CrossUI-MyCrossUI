2c5f36263b52ec5e4cd5762be5fb2b8b5f201a66
nickantonaccio <nick@guitarz.org>
2022-12-11 22:59:18 -0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,150 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.TreeGrid")
+                .setHost(host,"xui_ui_treegrid2")
+                .setLeft("0em")
+                .setTop("0em")
+                .setRowNumbered(true)
+                .setHeader([
+                    {
+                        "id" : "col1",
+                        "width" : "8em",
+                        "type" : "input",
+                        "caption" : "col1"
+                    },
+                    {
+                        "id" : "col2",
+                        "width" : "8em",
+                        "type" : "input",
+                        "caption" : "col2"
+                    },
+                    {
+                        "id" : "col3",
+                        "width" : "8em",
+                        "type" : "input",
+                        "caption" : "col3"
+                    },
+                    {
+                        "id" : "col4",
+                        "width" : "8em",
+                        "type" : "input",
+                        "caption" : "col4"
+                    }
+                ])
+                .setRows([
+                    {
+                        "cells" : [
+                            {
+                                "value" : "A1"
+                            },
+                            {
+                                "value" : "B1"
+                            },
+                            {
+                                "value" : "C1"
+                            },
+                            {
+                                "value" : "D1"
+                            }
+                        ]
+                    },
+                    {
+                        "cells" : [
+                            {
+                                "value" : "A2"
+                            },
+                            {
+                                "value" : "B2"
+                            },
+                            {
+                                "value" : "C2"
+                            },
+                            {
+                                "value" : "D2"
+                            }
+                        ]
+                    },
+                    {
+                        "cells" : [
+                            {
+                                "value" : "A3"
+                            },
+                            {
+                                "value" : "B3"
+                            },
+                            {
+                                "value" : "C3"
+                            },
+                            {
+                                "value" : "D3"
+                            }
+                        ],
+                        "sub" : [
+                            {
+                                "cells" : [
+                                    "A4",
+                                    "B4",
+                                    "C4",
+                                    "D4"
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "iniFold" : false,
+                        "cells" : [
+                            {
+                                "value" : "A5"
+                            },
+                            {
+                                "value" : "B5"
+                            },
+                            {
+                                "value" : "C5"
+                            },
+                            {
+                                "value" : "D5"
+                            }
+                        ],
+                        "sub" : [
+                            {
+                                "cells" : [
+                                    {
+                                        "value" : "A6"
+                                    },
+                                    {
+                                        "value" : "B6"
+                                    },
+                                    {
+                                        "value" : "C6"
+                                    },
+                                    {
+                                        "value" : "D6"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ])
+                .onClickCell([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "other",
+                        "target" : "msg",
+                        "args" : [
+                            "test",
+                            "test1"
+                        ],
+                        "method" : "alert",
+                        "onOK" : 2,
+                        "event" : 2
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
